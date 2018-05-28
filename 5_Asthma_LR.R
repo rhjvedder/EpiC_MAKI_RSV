720b3269d5c61d3a0a95d031a9da759f31182c51
rhjvedder <rolf.vedder@gmail.com>
2018-05-28 15:30:24 +0200

    added png settings also applied png settings to Asthma LR

--- 5_Asthma_LR.R
+++ 5_Asthma_LR.R
@@ -20,9 +20,8 @@ loc.rgdata <- "Data/RG_data"
 loc.comp <- "Complementary_data"
 loc.mdata <- "Data/M_Values"
 loc.model <- "Data/Models/model_1_Asthma"
-font.mp <- 2
 setwd(loc)
-
+load(file=paste(loc.comp, "png_settings.Rdata", sep="/"))
 load(file=paste(loc.mdata, "MAKI_trimmed_M.Rdata", sep="/"))
 M_matrix<- M.val
 phenotype <- read.csv(paste(loc.comp, "Phenotype_data_Maki.csv", sep = "/"))
@@ -88,7 +87,7 @@ library(GWASTools)
 pvalue<- all.results[,4]
 names(pvalue)<- as.character(all.results[,1])
 padjust<- p.adjust(pvalue,"fdr")
-png(file=paste(loc.model, paste0("qqplot_",filename,".png"), sep="/"), width=1200, height=1000)
+png(file=paste(loc.model, paste0("qqplot_",filename,".png"), sep="/"), width=png.w, height=png.h)
 qqPlot(pvalue, main="QQ of methylation model Asthma = Meth + Batch", cex.lab=font.mp, cex.axis=font.mp, cex.main=font.mp, cex.sub=font.mp)
 t <- estlambda(pvalue, method="median",plot=F)
 t <- t[[1]]
